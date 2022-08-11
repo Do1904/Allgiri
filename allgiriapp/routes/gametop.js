9c66c4a404731b9d93e87c1538b9eb039188c320
近藤亮太 <a8120083@gsuite.si.aoyama.ac.jp>
2022-08-11 18:53:37 +0900

    session stuff completed. yey.

--- allgiriapp/routes/gametop.js
+++ allgiriapp/routes/gametop.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const knex = require('../db/knex');
 
-router.get('/gametop', function(req, res, next) {
-  res.render('gametop', { title: 'Allgiri App' });
+router.get('/', function (req, res, next) {
+  res.render('gametop', {
+      title: 'Game Top',
+  });
 });
-
 
 module.exports = router;
--- allgiriapp/routes/index.js
+++ allgiriapp/routes/index.js
@@ -3,11 +3,30 @@ var router = express.Router();
 const knex = require('../db/knex');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Allgiri App' });
+router.get('/', function (req, res, next) {
+  const userId = req.session.userid;
+  const isAuth = Boolean(userId);
+  console.log(`isAuth: ${isAuth}`);
+
+  res.render('index', {
+    title: 'Allgiri App',
+    isAuth: isAuth
+  });
+});
+
+router.post('/', function (req, res, next) {
+  const userId = req.session.userid;
+  const isAuth = Boolean(userId);
+
+  res.render('index', {
+    title: 'Allgiri App',
+    isAuth: isAuth
+  });
 });
 
 router.use('/signup', require('./signup'));
+router.use('/signin', require('./signin'));
+router.use('/signout', require('./signout'));
 router.use('/gametop', require('./gametop'));
 
 module.exports = router;
